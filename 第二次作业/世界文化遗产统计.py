d762db58cb3cd0fe4cb3490920321e6dc8576a44
FangXzhong <64191471+FangXzhong@users.noreply.github.com>
2021-04-05 13:28:19 +0800

    Update 世界文化遗产统计.py

--- 第二次作业/世界文化遗产统计.py
+++ 第二次作业/世界文化遗产统计.py
@@ -9,20 +9,23 @@ import pyecharts.options as opts
 
 from 世界地图国家中英文对照 import my_dict
 
+# 以下两句代码就是生成一个中英文国家名称对照的字典
 Chinese_English_dict = my_dict
 English_Chinese_dict = {Chinese_English_dict[item]: item
                         for item in Chinese_English_dict.keys()}
 
+# 从xlsx文件读入数据并进行处理输出
 file = xlrd.open_workbook('./data/各国世界文化遗产（截止2019年7月10日）.xlsx')
 table = file.sheet_by_index(0)
 data = []
 for i in range(table.nrows):
     if i == 0:
-        continue
+        continue # 跳过标题行
     data.append([English_Chinese_dict[table.row_values(i)[0].strip()],
                  int(table.row_values(i)[1])])
 print(data)
 
+# 开始画图
 my_map = charts.Map()
 my_map.add('世界文化遗产数量', data, "world")\
     .set_series_opts(label_opts=opts.LabelOpts(is_show=False))\
